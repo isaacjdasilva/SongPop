361edd26076e8d478aa86019f1888fc8d7bcaacc
waldirbb <waldirbb@yahoo.com.br>
2012-10-27 15:12:22 -0200

    Funções de javaScript separadas em arquivos

--- frontend/funcao.js
+++ frontend/funcao.js
@@ -1,3 +1,32 @@
-$(document).ready(function() {	
-		alert("Olá mundo!");
+/*! Funções da tela de batalha */
+
+$(document).ready(function() {
+	$("#opcao1")
+		.mouseover(function() { 
+			$("#opcao1").css("border-color","red");
+		})
+		.mouseout(function() {
+			$("#opcao1").css("border-color","white");
+		});
+	$("#opcao2")
+		.mouseover(function() { 
+			$("#opcao2").css("border-color","red");
+		})
+		.mouseout(function() {
+			$("#opcao2").css("border-color","white");
+		});
+	$("#opcao3")
+		.mouseover(function() { 
+			$("#opcao3").css("border-color","red");
+		})
+		.mouseout(function() {
+			$("#opcao3").css("border-color","white");
+		});
+	$("#opcao4")
+		.mouseover(function() { 
+			$("#opcao4").css("border-color","red");
+		})
+		.mouseout(function() {
+			$("#opcao4").css("border-color","white");
+		});
 });
